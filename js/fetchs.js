161792f0253d8130234efb03799fd078efc90b7a
gabriela diaz <gbdadx@gmail.com>
2023-09-27 10:29:50 -0300

    no more dark mode with bootstrap, it doesnt work with this code

---
+++ js/fetchs.js
@@ -0,0 +1,11 @@
+/*funciona usando el fetch contra el json o contra la api */
+
+//let enlace = 'https://restcountries.com/v3.1/all';
+let enlace = './countries.json';
+
+  fetch(enlace)
+    .then(response => response.json())
+    .then(data =>{
+		localStorage.setItem("objeto", JSON.stringify(data));
+	})
+	.catch(error => console.error('Error:', error));
--- js/index.js
+++ js/index.js
@@ -1,17 +1,7 @@
 /**
  * leer el localstorage: objeto
  */
-/*funciona usando el fetch contra el json o contra la api */
 
-//let enlace = 'https://restcountries.com/v3.1/all';
-let enlace = './countries.json';
-
-  fetch(enlace)
-    .then(response => response.json())
-    .then(data =>{
-		localStorage.setItem("objeto", JSON.stringify(data));
-	})
-	.catch(error => console.error('Error:', error));
 
 
 const data = JSON.parse(localStorage.getItem('objeto'));
