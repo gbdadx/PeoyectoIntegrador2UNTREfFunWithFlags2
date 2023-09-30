3245c0fe81c8e997feb8748a424b5741d81fda06
gabriela diaz <gbdadx@gmail.com>
2023-09-29 22:05:09 -0300

    changed the json, for independent countries true

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /*funciona usando el fetch contra el json o contra la api */
 
 //let enlace = 'https://restcountries.com/v3.1/all';
-//let enlace = './countries.json';
+//let enlace = './independent.json';
 let enlace = 'https://restcountries.com/v3.1/independent?status=true';
 
 fetch(enlace)
