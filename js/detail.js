3d576ed088152d9552cba29566ee0c7bb78e9d50
gabriela diaz <gbdadx@gmail.com>
2023-10-04 13:26:05 -0300

    added a blind popup to map

--- js/detail.js
+++ js/detail.js
@@ -24,17 +24,17 @@ function fillingAsideDetails(pais) {
     const wiki = `https://en.wikipedia.org/wiki/${pa}`;
     const small6 = wiki;
     console.log(`capital  ${small1}`);
-const anthem= pais.cca2;
-const linkanthem=`https://nationalanthems.info/${anthem}.htm`;
-const small7= linkanthem;
-const demonyms= pais.demonyms.eng.m;
+    const anthem = pais.cca2;
+    const linkanthem = `https://nationalanthems.info/${anthem}.htm`;
+    const small7 = linkanthem;
+    const demonyms = pais.demonyms.eng.m;
     const sma1 = document.querySelector('.small1');
     const sma2 = document.querySelector('.small2');
     const sma3 = document.querySelector('.small3');
     const sma4 = document.querySelector('.small4');
     const sma5 = document.querySelector('.small5');
     const sma6 = document.querySelector('.small6');
-    const sma7= document.querySelector('.small7');
+    const sma7 = document.querySelector('.small7');
     sma1.innerHTML = `<p><span>Capital:</span> <span>   ${small1}  </span>  </p>`;
     sma2.innerHTML = `<p><span>Start Of Week:</span> <span>   ${small2}  </span>  </p>`;
     sma3.innerHTML = `<p><span>Population:</span> <span>   ${small3}  </span>  </p>`;
@@ -42,32 +42,34 @@ const demonyms= pais.demonyms.eng.m;
     sma5.innerHTML = `<p> <a href=" ${small5}" target="_blank">Cost Of Living (NUMBEO)</a>    </p>`;
     sma6.innerHTML = `<p> <a href=" ${small6}" target="_blank">Wikipedia ${(pa)}</a>    </p>`;
     sma7.innerHTML = `<p> <a href=" ${small7}" target="_blank">National ${(demonyms)} Anthem</a>    </p>`;
-console.log(`google maps address: ${small4}`)
-console.log(`openStreetMaps  ${pais.maps.openStreetMaps}`)
+    console.log(`google maps address: ${small4}`)
+    console.log(`openStreetMaps  ${pais.maps.openStreetMaps}`)
 
-/** trabajo con mapa y leafleat */
-/*let latitud=pais.latlng[0];
-let longitud=pais.latlng[1];
-let enlaceMapa=pais.maps.openStreetMaps;
-var map = L.map('map').setView([latitud, longitud], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);*/
+    /** trabajo con mapa y leafleat */
+    /*let latitud=pais.latlng[0];
+    let longitud=pais.latlng[1];
+    let enlaceMapa=pais.maps.openStreetMaps;
+    var map = L.map('map').setView([latitud, longitud], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);*/
 
-// Trabajar con mapa y Leaflet
-let latitud = pais.latlng[0];
-let longitud = pais.latlng[1];
-let enlaceMapa = pais.maps.openStreetMaps;
+    // Trabajar con mapa y Leaflet
+    let latitud = pais.latlng[0];
+    let longitud = pais.latlng[1];
+    let enlaceMapa = pais.maps.openStreetMaps;
 
-var map = L.map('map').setView([latitud, longitud], 6);
+    var map = L.map('map').setView([latitud, longitud], 6);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 13,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 13,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-
+    var marker = L.marker([latitud, longitud],
+        {alt: pa}).addTo(map) // "Kyiv" is the accessible name of this marker
+        .bindPopup(`${pa}`);
 
 
 }
